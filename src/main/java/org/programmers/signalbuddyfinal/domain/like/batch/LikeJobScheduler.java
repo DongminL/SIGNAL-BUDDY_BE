fd22ec4d48d7fe424921bbfa6f27afc68566d387
DongminL <milkskfk12@gmail.com>
2026-05-03 12:25:56 +0900

    fix: batch job이 중복되는 문제 방지

--- src/main/java/org/programmers/signalbuddyfinal/domain/like/batch/LikeJobScheduler.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/like/batch/LikeJobScheduler.java
@@ -5,6 +5,7 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,6 +16,7 @@ public class LikeJobScheduler {
 
     private final JobLauncher jobLauncher;
     private final Job likeRequestJob;
+    private final JobExplorer jobExplorer;
 
     @Scheduled(cron = "${schedule.like-job.cron}")
     @SchedulerLock(
@@ -22,7 +24,8 @@ public class LikeJobScheduler {
         lockAtMostFor = "${schedule.like-job.lockAtMostFor}",
         lockAtLeastFor = "${schedule.like-job.lockAtLeastFor}")
     public void runJob() throws Exception {
-        JobParameters params = new JobParametersBuilder()
+        JobParameters params = new JobParametersBuilder(jobExplorer)
+            .getNextJobParameters(likeRequestJob)
             .toJobParameters();
         jobLauncher.run(likeRequestJob, params);
     }
